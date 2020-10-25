853137fea99959fd1d0e7d918f9b3b27c195e0db
Antonio-rl <antonioriosleiva@protonmail.com>
2020-10-25 02:08:52 -0500

    terminado

--- Clases/estructura_repetitivas/IdentificarNumeroMayor.py
+++ Clases/estructura_repetitivas/IdentificarNumeroMayor.py
@@ -1,15 +1,15 @@
 def IdentificadorNumeroMayor():
-    pytho#Datos entrada
-  num1=int(input("Ingrese el Primer valor numerico"))
-  num2=int(input("Ingrese el Segundo valor numerico"))
-  num3=int(input("Ingrese el Tercer valor numerico"))
+    #Datos entrada
+  num1=int(input("Ingrese el Primer valor numerico:"))
+  num2=int(input("Ingrese el Segundo valor numerico:"))
+  num3=int(input("Ingrese el Tercer valor numerico:"))
     #Proceso
   if(num1>num2 and num1>num3):
     numMayor=num1
   elif num2>num1 and num2>num3:
     numMayor=num2
   else:
-  numMayor=num3
+    numMayor=num3
   #Datos Salida
   print("El numero mayor es:",numMayor)
 
--- Mis_programas/npositivo_nnegativo.py
+++ Mis_programas/npositivo_nnegativo.py
@@ -3,8 +3,8 @@ def npositivo_nnegativo():
   #proceso
   if numero>0:
     print("El numero es positivo.")
- elif 0==0 :
+  elif 0==0 :
     print("Igual a 0.")
- else:
-  print "El numero es negativo."
-npositivo_nnegativo():
+  else:
+    print("El numero es negativo.")
+npositivo_nnegativo()
